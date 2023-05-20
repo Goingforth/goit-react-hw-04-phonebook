4fcc590eb8d6768e1ee95681d29f121c1243ac41
Oleksandr Siukov <as-service2008@ukr.net>
2023-05-20 13:12:03 +0300

    Update App.jsx

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import { nanoid } from 'nanoid';
 import ContactForm from 'components/ContactForm/ContactForm';
 import { Container } from './Container.styled';
@@ -6,13 +6,18 @@ import Filter from 'components/Filter/Filter';
 import ContactList from 'components/ContactList/ContactList';
 
 function App() {
-  const [contacts, setContacts] = useState([
-    { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
-    { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
-    { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
-    { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
-  ]);
-  const [filter] = useState('');
+  const [contacts, setContacts] = useState(() => {
+    return (
+      JSON.parse(window.localStorage.getItem('contacts')) ?? [
+        { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
+        { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
+        { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
+        { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
+      ]
+    );
+  });
+
+  const [filter, setFilter] = useState('');
 
   const createContact = data => {
     const newContact = {
@@ -26,36 +31,33 @@ function App() {
       : setContacts(contacts.concat(newContact));
   };
 
-  // updateFilter = evt => {
-  //   this.setState({ filter: evt.target.value });
-  // };
-  // newContacts = () => {
-  //   return this.state.contacts.filter(el =>
-  //     el.name.toLowerCase().includes(this.state.filter.toLowerCase())
-  //   );
-  // };
+  const updateFilter = evt => {
+    setFilter(evt.target.value);
+  };
+
+  const filteredContacts = () => {
+    return contacts.filter(el =>
+      el.name.toLowerCase().includes(filter.toLowerCase())
+    );
+  };
 
   const deleteContact = id =>
     setContacts(contacts.filter(contact => contact.id !== id));
 
-  // componentDidMount() {
-  //   const contactsInitial = localStorage.getItem('contacts');
-  //   this.setState({ contacts: JSON.parse(contactsInitial) });
-  // }
-  // componentDidUpdate(prevProps, prevState) {
-  //   if (prevState.contacts !== this.state.contacts) {
-  //     localStorage.setItem('contacts', JSON.stringify(this.state.contacts));
-  //   }
-  // }
+  useEffect(() => {
+    window.localStorage.setItem('contacts', JSON.stringify(contacts));
+  }, [contacts]);
 
-  //  const filteredContacts = this.newContacts();
   return (
     <Container>
       <h1>Phonebook</h1>
       <ContactForm createContact={createContact} />
       <h2>Contacts </h2>
-      {/* <Filter handleChange={updateFilter} value={filter} /> */}
-      <ContactList contacts={contacts} onDeleteContact={deleteContact} />
+      <Filter handleChange={updateFilter} value={filter} />
+      <ContactList
+        contacts={filteredContacts()}
+        onDeleteContact={deleteContact}
+      />
     </Container>
   );
 }
